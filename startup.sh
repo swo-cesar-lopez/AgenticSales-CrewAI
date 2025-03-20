edc35e8737738efba546226606a7693c6ea1746c
Cesar Lopez <clopeza@outlook.com>
2025-03-20 17:48:54 -0500

    Se ajusta el Startup para el inicio del proyecto

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/site/wwwroot
-gunicorn --workers 2 --timeout 600 --bind=0.0.0.0:8000 src.crew_ai_agents.app:app
+gunicorn -w 2 -k uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 src.crew_ai_agents.app:app
